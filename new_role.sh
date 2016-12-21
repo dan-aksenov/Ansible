9a20a0370bc5a1901f649fba5856343ed75c463f
dan-aksenov <dan-aksenov@yandex.ru>
2016-12-21 14:10:20 +0300

    lots of stuff new_role modified, yum-cron cloned

--- new_role.sh
+++ new_role.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "role name var is unset" && exit 1; fi
 touch $1.yml
-mkdir $1
+cat > $1.yml << EOF
+- hosts: all
+  user: dbax
+  become: yes
+
+  roles:
+      - $1 
+EOF
+mkdir roles/$1
 cd $1
 mkdir tasks handlers templates files vars defaults meta 
 touch tasks/main.yml
