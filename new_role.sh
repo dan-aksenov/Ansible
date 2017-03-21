908d69cf475c69c661ab7210c2d7a9da89c9300d
dan-aksenov <dan-aksenov@yandex.ru>
2017-03-21 15:16:37 +0300

    metadata template added

--- new_role.sh
+++ new_role.sh
@@ -9,3 +9,14 @@ mkdir tasks handlers templates files vars defaults meta
 # Create sample files in all role's subdirectories
 for i in $(ls); do  echo --- > $i/main.yml; done
 for i in $(ls); do  echo ... >> $i/main.yml; done
+
+cat > meta/main.yml <<EOF
+galaxy_info:
+  author: Daniil Aksenov (dbax)
+  description: Ansible role for administrative sudo user, who will run all subsequent ansible scripts. Must be executed with --ask-pass.
+  license: as-is
+  platforms:
+      name: EL
+      versions: 7
+  categories:
+EOF
