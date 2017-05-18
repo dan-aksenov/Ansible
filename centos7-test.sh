adbeeadcb320ef0d0a6b5fa70bae6f559ae78688
dan-aksenov <dan-aksenov@yandex.ru>
2017-05-18 17:29:42 +0300

    plpgsql_check redone

--- centos7-test.sh
+++ centos7-test.sh
@@ -4,17 +4,17 @@ pgver=9.6
 
 clear
 
-read -p "Initialize default ansible user (root password will be prompted)."
+read -p "Initialize default ansible user (root password will be prompted)"
 ansible-playbook -i test init.yml --ask-pass
 
 read -p "Setup standart config for new Centos7 machines."
 ansible-playbook -i test new_machine.yml
 
-read -p "Setup static ip address."
+read -p "Setup static ip address"
 ansible-playbook -i test static_ip.yml
 
-read -p "Install and configure postgresql 9.6."
+read -p "Install and configure postgresql 9.6"
 ansible-playbook -i test pg_main.yml --extra-vars "postgresql_version=$pgver"
 
-read -p "Install postgresql's optional extensions."
-ansible-playbook -i test pg_misc.yml --tags oracle_fdw,pgbadger --extra-vars "postgresql_version=$pgver"
+read -p "Install postgresql's optional extensions"
+ansible-playbook -i test pg_misc.yml --tags oracle_fdw,pgbadger,plpgsql_check --extra-vars "postgresql_version=$pgver"
