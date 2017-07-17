df06ea2711d4a7a858df8e0ae94553a973005b20
dan-aksenov <dan-aksenov@yandex.ru>
2017-07-17 16:50:14 +0300

    open info file after creation.

--- new_role.sh
+++ new_role.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "role name var is unset" && exit 1; fi
 
-# Create role's main directorie
+# Create role's main directory.
 mkdir roles/$1
 cd roles/$1
-# Create role's subdirectories
+# Create role's subdirectories.
 mkdir tasks handlers templates files vars defaults meta 
-# Create sample files in all role's subdirectories
+# Create dummy files in all role's subdirectories.
 for i in $(ls); do  echo --- > $i/main.yml; done
 for i in $(ls); do  echo ... >> $i/main.yml; done
 
+# Create template info file in meta.
 cat > meta/main.yml <<EOF
 galaxy_info:
   author: Daniil Aksenov (dbax)
@@ -21,3 +22,6 @@ galaxy_info:
   categories:
   dependencies:
 EOF
+
+# Open info file for editing.
+vim meta/main.yml
