8cc5389e712950975b8cd90814ccf7235c1cb8ef
dan-aksenov <dan-aksenov@yandex.ru>
2017-07-28 13:39:03 +0300

    test.yml added with simple run

--- new_role.sh
+++ new_role.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 if [ -z "$1" ]; then echo "role name var is unset" && exit 1; fi
 
+role=$1
+
 # Create role's main directory.
-mkdir roles/$1
-cd roles/$1
+mkdir roles/$role
+cd roles/$role
 # Create role's subdirectories.
-mkdir tasks handlers templates files vars defaults meta
+mkdir tasks handlers templates files vars defaults meta tests
 # Create dummy files in all role's subdirectories.
 for i in $(ls); do  echo --- > $i/main.yml; done
 for i in $(ls); do  echo '#This is dummy file to make shure directory is commited. Can be deleted after something usefull added to this dir.' > $i/main.yml; done
 for i in $(ls); do  echo ... >> $i/main.yml; done
+
+# Create sample test file
+cat > tests/tests.yml <<EOF
+- hosts: all
+  remote_user: "{{ my_ansible_user }}"
+  become: yes
+  roles:
+    - $role
+EOF
 
 # Create template info file in meta.
 cat > meta/main.yml <<EOF
