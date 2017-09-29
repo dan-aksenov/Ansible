8cf411bda3dd76a9fc9cf4a05d9a18948b8fd82d
dan-aksenov <dan-aksenov@yandex.ru>
2017-09-29 17:03:00 +0300

    static ip commented out

--- centos7-test.sh
+++ centos7-test.sh
@@ -10,8 +10,8 @@ ansible-playbook -i test init.yml --ask-pass
 read -p "Setup standart config for new Centos7 machines."
 ansible-playbook -i test new_machine.yml
 
-read -p "Setup static ip address"
-ansible-playbook -i test static_ip.yml
+#read -p "Setup static ip address"
+#ansible-playbook -i test static_ip.yml
 
 read -p "Install and configure postgresql 9.6"
 ansible-playbook -i test pg_main.yml --extra-vars "postgresql_version=$pgver"
