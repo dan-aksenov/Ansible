6442d9eabcc4b6a67e78ffe5b3e5fb862f8150df
Аксёнов Даниил <daniil.aksenov@fors.ru>
2018-02-20 16:32:11 +0300

    zabbix-pyora-activecheck started

--- new_role.sh
+++ new_role.sh
@@ -3,8 +3,9 @@ if [ -z "$1" ]; then echo "Role name var is unset. Set it as first positional pa
 
 role=$1
 
+cd roles
 # Create ansible-galaxy stile role.
 ansible-galaxy init $role
 
 # Open info file for editing.
-vim roles/$role/meta/main.yml
+vim $role/meta/main.yml
