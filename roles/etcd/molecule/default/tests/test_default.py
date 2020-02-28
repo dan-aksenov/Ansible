4459536f68611bd8771fb316c438e2244bcc95e3
dbax <dan-aksenov@yandex.ru>
2020-02-28 12:03:16 +0300

    testinfra added

--- roles/etcd/molecule/default/tests/test_default.py
+++ roles/etcd/molecule/default/tests/test_default.py
@@ -1,5 +1,7 @@
 import os
 
+import testinfra
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -7,9 +9,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_etcd_service(host):
+    etcd = host.service('etcd')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert etcd.is_running
+    assert etcd.is_enabled
+
+
+def test_etcd_health(host):
+    cmd = host.run("etcdctl cluster-health")
+
+    assert cmd.succeeded
+    assert 'etcd1' in cmd.stdout
+    assert 'etcd2' in cmd.stdout
+    assert 'etcd3' in cmd.stdout
+    assert 'cluster is healthy' in cmd.stdout
