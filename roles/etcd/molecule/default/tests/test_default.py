8e2c0ebd7479b27deb61505c1343543cabf3fe92
dbax <dan-aksenov@yandex.ru>
2020-02-28 15:25:02 +0300

    1node(default) and 3node testinfra tests added

--- roles/etcd/molecule/default/tests/test_default.py
+++ roles/etcd/molecule/default/tests/test_default.py
@@ -14,13 +14,3 @@ def test_etcd_service(host):
 
     assert etcd.is_running
     assert etcd.is_enabled
-
-
-def test_etcd_health(host):
-    cmd = host.run("etcdctl cluster-health")
-
-    assert cmd.succeeded
-    assert 'etcd1' in cmd.stdout
-    assert 'etcd2' in cmd.stdout
-    assert 'etcd3' in cmd.stdout
-    assert 'cluster is healthy' in cmd.stdout
