a48ef4613837c3ed349a66426423a7ea358d2ae7
dbax <dan-aksenov@yandex.ru>
2020-07-27 13:41:42 +0300

    powa role separated

---
+++ roles/postgres_powa/molecule/default/tests/test_default.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+@pytest.fixture(scope='module')
+def AnsibleRoleDefaults(host):
+    return host.ansible('include_vars', '../../defaults/main.yml')['ansible_facts']
+
+
+def test_pgdata(host, AnsibleRoleDefaults):
+    postgresql_version = AnsibleRoleDefaults['postgresql_version']
+    
+    # pg_data needs to be constructed including postgresql_version. How to do in in testinfra?
+    f = host.file('/var/lib/pgsql/' + str(postgresql_version) + '/data')
+
+    assert f.exists
+    assert f.is_directory
+    assert f.user == 'postgres'
+    assert f.group == 'postgres'
+
+
+def test_etcd_service(host, AnsibleRoleDefaults):
+
+    postgresql_version = AnsibleRoleDefaults['postgresql_version']
+    
+    postgresql_service = host.service('postgresql-' + str(postgresql_version))
+
+    assert postgresql_service.is_running
+    assert postgresql_service.is_enabled
