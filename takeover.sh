f54e4e5a5dd7a9f451486c075b4f86f060ec4a40
dbax <dan-aksenov@yandex.ru>
2020-07-31 15:30:08 +0300

    takeover script modified

--- takeover.sh
+++ takeover.sh
@@ -5,4 +5,9 @@
 
 INVENTORY=$1
 HOST=$2
+# Get current postgresql settings.
 ansible -i $INVENTORY $HOST -m postgresql_info --become --become-user=postgres -a "filter=settings" -t /tmp 
+
+# Parse /tmp/$HOST for modified PostgreSQL settings.
+
+# Generate ansible host_vars list with gathered values.
